c53d847b452d134f5d426a8a95d82c5ea1cf891b
Achraf ATIA <a.atia@b3gtech.com>
2023-08-30 11:25:00 +0100

    set Models Relationships and configure SQL Server

--- eTickets/Models/Cinema.cs
+++ eTickets/Models/Cinema.cs
@@ -1,6 +1,16 @@
-﻿namespace eTickets.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace eTickets.Models
 {
     public class Cinema
     {
+        [Key]
+        public int CinemaId { get; set; }
+        public string CinemaLogo { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+
+        //Relationships
+        public List<Movie> Movies { get; set; }
     }
 }
--- eTickets/Models/Producer.cs
+++ eTickets/Models/Producer.cs
@@ -5,9 +5,12 @@ namespace eTickets.Models
     public class Producer
     {
         [Key]
-        public int Producer_Id { get; set; }
+        public int ProducerId { get; set; }
         public string ProfilePictureURL { get; set; }
         public string FullName { get; set; }
         public string Bio { get; set; }
+
+        //Relationships
+        public List<Movie> Movies { get; set; }
     }
 }
